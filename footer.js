4414bb95104d7aa6d29a61a7a1daa8e545ad340f
sreejith <viswambaran8@gmail.com>
2025-12-01 02:45:43 +0530

    IT Solutions

---
+++ footer.js
@@ -0,0 +1,19 @@
+VANTA.BIRDS({
+  el: "#vanta-fr",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+
+  backgroundColor: 0xffffff,   // white background
+  color1: 0x0a7a55,            // green gradient color 1
+  color2: 0x075544,            // green gradient color 2
+
+  birdSize: 2.00,
+  wingSpan: 20.00,
+  speedLimit: 8.00,
+  separation: 15.00   // lower value = more birds
+});
